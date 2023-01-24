e18af10eee3cfae5f1adc8d601ebf6404ecfe427
ddeo1301 <divyanshudeo1301@gmail.com>
2023-01-24 21:06:49 +0530

    Dom practise

--- dom.js
+++ dom.js
@@ -1,7 +1,10 @@
-var odd = document.querySelectorAll('li:nth-child(odd');
-var even = document.querySelectorAll('li:nth-child(even')
+// var odd = document.querySelectorAll('li:nth-child(odd');
+// var even = document.querySelectorAll('li:nth-child(even')
 
-for(var i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = '#f4f4f4';
-    even[i].style.backgroundColor = '#ccc';
-}
+// for(var i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = 'green';
+//     even[i].style.backgroundColor = '#ccc';
+// }
+
+// var pn = document.querySelector('Head');
+// pn.innerHTML = <li>HEllo</li> + pn.innerHTML;
